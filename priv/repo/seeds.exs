a9205e6108cdde9bb2ea65bbc34cb09fbae5fed0
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-15 20:17:38 +0100

    fix(backend): seed - prevent data duplication

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,31 +1,32 @@
-{:ok, job} = Wttj.Jobs.create_job(%{name: "Full Stack Developer"})
+try do
+  Wttj.Jobs.get_job!(1)
+  IO.puts("Job with ID 1 already exists. No new data created.")
+rescue
+  Ecto.NoResultsError ->
+    IO.puts("Seeding data...")
+    {:ok, job} = Wttj.Jobs.create_job(%{name: "Full Stack Developer"})
 
-candidate =
-  Wttj.Candidates.create_candidate(%{job_id: job.id, email: "user1@wttj.co", position: 0})
+    Wttj.Candidates.create_candidate(%{job_id: job.id, email: "user1@wttj.co", position: 0})
+    Wttj.Candidates.create_candidate(%{job_id: job.id, email: "user2@wttj.co", position: 1})
 
-candidate =
-  Wttj.Candidates.create_candidate(%{job_id: job.id, email: "user2@wttj.co", position: 1})
+    Wttj.Candidates.create_candidate(%{
+      job_id: job.id,
+      email: "user3@wttj.co",
+      position: 0,
+      status: :interview
+    })
 
-candidate =
-  Wttj.Candidates.create_candidate(%{
-    job_id: job.id,
-    email: "user3@wttj.co",
-    position: 0,
-    status: :interview
-  })
+    Wttj.Candidates.create_candidate(%{
+      job_id: job.id,
+      email: "user4@wttj.co",
+      position: 0,
+      status: :rejected
+    })
 
-candidate =
-  Wttj.Candidates.create_candidate(%{
-    job_id: job.id,
-    email: "user4@wttj.co",
-    position: 0,
-    status: :rejected
-  })
-
-candidate =
-  Wttj.Candidates.create_candidate(%{
-    job_id: job.id,
-    email: "user5@wttj.co",
-    position: 1,
-    status: :rejected
-  })
+    Wttj.Candidates.create_candidate(%{
+      job_id: job.id,
+      email: "user5@wttj.co",
+      position: 1,
+      status: :rejected
+    })
+end
