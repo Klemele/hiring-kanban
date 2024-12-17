b046dd6ac021d0ad35263bdec1d31d526b3da5f3
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-17 15:02:02 +0100

    feat(backend): generate job socket & channel with `mix phx.gen.`

---
+++ test/wttj_web/channels/job_channel_test.exs
@@ -0,0 +1,27 @@
+defmodule WttjWeb.JobChannelTest do
+  use WttjWeb.ChannelCase
+
+  setup do
+    {:ok, _, socket} =
+      WttjWeb.UserSocket
+      |> socket("user_id", %{some: :assign})
+      |> subscribe_and_join(WttjWeb.JobChannel, "job:lobby")
+
+    %{socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push(socket, "ping", %{"hello" => "there"})
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to job:lobby", %{socket: socket} do
+    push(socket, "shout", %{"hello" => "all"})
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from!(socket, "broadcast", %{"some" => "data"})
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
