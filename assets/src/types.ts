520d7641371ddf1548981807cdad450b01645459
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-17 10:18:03 +0100

    chore(frontend): move SortedCandidated to types

--- assets/src/types.ts
+++ assets/src/types.ts
@@ -1,1 +1,11 @@
+import { Candidate } from './api'
+
 export type Statuses = 'new' | 'interview' | 'hired' | 'rejected'
+
+export interface SortedCandidates {
+  [key: string]: Candidate[]
+  new: Candidate[]
+  interview: Candidate[]
+  hired: Candidate[]
+  rejected: Candidate[]
+}
