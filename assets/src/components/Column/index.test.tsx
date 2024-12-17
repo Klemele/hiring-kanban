29be6d0fc6b22bbb716eb442a91481a855745ea6
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-17 13:37:04 +0100

    feat(frontend): move candidate list to dedicated component

--- assets/src/components/Column/index.test.tsx
+++ assets/src/components/Column/index.test.tsx
@@ -1,21 +1,16 @@
 import { expect, test } from 'vitest'
 
-import { Text } from '@welcome-ui/text'
 import Column from '.'
-import { Candidate } from '../../api'
 import { generateCandidates, render } from '../../test-utils'
 import { Statuses } from '../../types'
-import CandidateCard from '../Candidate'
-import DragItem from '../DragItem'
+import CandidateList from '../CandidateList'
 
 const COLUMNS: Statuses[] = ['new', 'interview', 'hired', 'rejected']
 
 test('renders column name', () => {
   const { getByText } = render(
     <Column name={COLUMNS[0]} size={0}>
-      <Text color="neutral-40" textAlign="center">
-        No candidates
-      </Text>
+      <CandidateList candidates={[]} />
     </Column>
   )
   expect(getByText('new')).toBeInTheDocument()
@@ -25,11 +20,7 @@ test('renders column with candidates', () => {
   const candidates = generateCandidates(10)
   const { getByText } = render(
     <Column name={COLUMNS[0]} size={candidates.length}>
-      {candidates.map((candidate: Candidate) => (
-        <DragItem key={candidate.id} id={candidate.id} data={candidate}>
-          <CandidateCard candidate={candidate} />
-        </DragItem>
-      ))}
+      <CandidateList candidates={candidates} />
     </Column>
   )
 
