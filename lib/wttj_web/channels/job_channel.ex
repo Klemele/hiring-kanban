b658e13fb46e12e24cb323277b473beff3da1a73
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-17 15:44:17 +0100

    feat(backend): jobs channel logic
    - get_candidates: retrieve list of candidates
    - update_candidate: update a candidate of a job, broadcast updated candidate on sucess

--- lib/wttj_web/channels/job_channel.ex
+++ lib/wttj_web/channels/job_channel.ex
@@ -1,32 +1,49 @@
 defmodule WttjWeb.JobChannel do
   use WttjWeb, :channel
 
+  alias Wttj.Candidates
+
   @impl true
-  def join("job:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  def join("jobs:" <> _job_id, _payload, socket) do
+    {:ok, socket}
   end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("get_candidates", %{"job_id" => job_id}, socket) do
+    candidates = Candidates.list_candidates(job_id)
+    push(socket, "candidates_data", %{candidates: candidates})
+    {:noreply, socket}
   end
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (job:lobby).
   @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
-  end
+  def handle_in(
+        "update_candidate",
+        %{"job_id" => job_id, "candidate" => candidate_params},
+        socket
+      ) do
+    case candidate_params do
+      %{"id" => id} ->
+        case Candidates.get_candidate!(job_id, id) do
+          nil ->
+            {:reply, {:error, "candidate_not_found"}, socket}
 
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
+          candidate ->
+            case Candidates.update_candidate(candidate, candidate_params) do
+              {:ok, updated_candidate} ->
+                broadcast!(socket, "candidate_updated", %{candidate: updated_candidate})
+                {:noreply, socket}
+
+              {:error, _changeset} ->
+                {:reply, {:error, "update_failed"}, socket}
+            end
+        end
+
+      _ ->
+        {:reply, {:error, "invalid_candidate_params"}, socket}
+    end
   end
 end
