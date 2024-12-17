bc95ae00caa153ffd7ecc80cd0f7a3ccd1e249fd
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-17 15:25:29 +0100

    fix(backend): candidate - add JSON encoder

--- lib/wttj/candidates/candidate.ex
+++ lib/wttj/candidates/candidate.ex
@@ -17,4 +17,14 @@ defmodule Wttj.Candidates.Candidate do
     |> cast(attrs, [:email, :status, :position, :job_id])
     |> validate_required([:email, :status, :position, :job_id])
   end
+
+  defimpl Jason.Encoder, for: Wttj.Candidates.Candidate do
+    def encode(%Wttj.Candidates.Candidate{} = candidate, opts) do
+      candidate
+      |> Map.from_struct()
+      # Drop any fields you don't want to serialize
+      |> Map.drop([:__meta__])
+      |> Jason.Encode.map(opts)
+    end
+  end
 end
